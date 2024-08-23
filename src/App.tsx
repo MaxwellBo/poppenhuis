26503287e9ea3e51a57baf7e6c29be4bc54eafea
Max Bo <mjbo@canva.com>
2024-08-23 22:23:17 +1000

    Revert "remove logging"

--- src/App.tsx
+++ src/App.tsx
@@ -491,6 +491,8 @@ export function WallLabelPage() {
 
 function computeModelSize(url: string): number | undefined {
   const resources = performance.getEntriesByType('resource');
+
+  console.log(resources);
   // @ts-ignore
   const resourceEntry: PerformanceResourceTiming | undefined = resources.find(entry => entry.name.endsWith(url));
   if (resourceEntry) {
