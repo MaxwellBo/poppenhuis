437ef881c8766d6150aadf032d4c14f48434b6c0
Max Bo <mjbo@canva.com>
2025-07-01 16:30:40 +1000

    Expose vars

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.COMMIT_REF': JSON.stringify(process.env.COMMIT_REF),
+    'import.meta.env.CONTEXT': JSON.stringify(process.env.CONTEXT),
+    'import.meta.env.DEPLOY_ID': JSON.stringify(process.env.DEPLOY_ID),
+  }
 })
