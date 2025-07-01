9067c53427b25aeb392e0e5278c918830fedf90a
Max Bo <mjbo@canva.com>
2025-07-01 12:17:09 +1000

    Add movement keys

--- src/components/AFrameScene.tsx
+++ src/components/AFrameScene.tsx
@@ -95,97 +95,102 @@ export const AFrameScene: React.FC<AFrameSceneProps> = ({ users, startingItem })
   const items = layout.filter(entity => entity.item).map(item => item.item!);
 
   return (
-    <a-scene embedded style={getStyleForModelSize('responsive-big')}>
-      <a-entity camera fly={true} look-controls wasd-controls="acceleration:100"  position={startingPosition}></a-entity>
-      <a-sky color="#fdf5e6"></a-sky>
-      <a-assets>
-        {items.map((item) => (
-              <a-asset-item key={item.id} id={item.id} src={item.model}></a-asset-item>
-          )
-        )}
-        {/* <img id="sky" src="clouds.webp"></img> */}
-      </a-assets>
-      {layout.map(({ position, item, user, collection, flip }) => {
-        if (user) {
-          if (flip) {
-            return (
-              <a-text 
-                value={user.name} 
-                color="#000" 
-                width="10"
-                rotation="0 -180 0"
-                position={computePosition(position)}
-              ></a-text>
-          ) } else {
-            return (
-              <a-text 
-                key={"flipped-user-" + user.id} 
-                value={user.name} 
-                color="#000" 
-                width="10"
-                rotation="0 0 0"
-                position={computePosition(position)}
-              ></a-text>
+    <div className='a-scene-wrapper'>
+      <div className='camera-keys'>
+        <kbd>W</kbd> <kbd>A</kbd> <kbd>S</kbd> <kbd>D</kbd>
+      </div>
+      <a-scene embedded style={getStyleForModelSize('responsive-big')}>
+        <a-entity camera fly={true} look-controls wasd-controls="acceleration:100"  position={startingPosition}></a-entity>
+        <a-sky color="#fdf5e6"></a-sky>
+        <a-assets>
+          {items.map((item) => (
+                <a-asset-item key={item.id} id={item.id} src={item.model}></a-asset-item>
             )
+          )}
+          {/* <img id="sky" src="clouds.webp"></img> */}
+        </a-assets>
+        {layout.map(({ position, item, user, collection, flip }) => {
+          if (user) {
+            if (flip) {
+              return (
+                <a-text 
+                  value={user.name} 
+                  color="#000" 
+                  width="10"
+                  rotation="0 -180 0"
+                  position={computePosition(position)}
+                ></a-text>
+            ) } else {
+              return (
+                <a-text 
+                  key={"flipped-user-" + user.id} 
+                  value={user.name} 
+                  color="#000" 
+                  width="10"
+                  rotation="0 0 0"
+                  position={computePosition(position)}
+                ></a-text>
+              )
+            }
           }
-        }
 
-        if (collection) {
-          if (flip) {
-            return (
-              <a-text
-                key={"flipped-collection-" + collection.id}
-                value={collection.name}
-                color="#000" 
-                width="5"
-                rotation="0 -180 0"
-                position={computePosition(position)}
-              ></a-text>
-            );
-          } else {
-            return (
-              <a-text
-                key={"collection-" + collection.id}
-                value={collection.name}
-                color="#000" 
-                width="5"
+          if (collection) {
+            if (flip) {
+              return (
+                <a-text
+                  key={"flipped-collection-" + collection.id}
+                  value={collection.name}
+                  color="#000" 
+                  width="5"
+                  rotation="0 -180 0"
+                  position={computePosition(position)}
+                ></a-text>
+              );
+            } else {
+              return (
+                <a-text
+                  key={"collection-" + collection.id}
+                  value={collection.name}
+                  color="#000" 
+                  width="5"
+                  rotation="0 0 0"
+                  position={computePosition(position)}
+                ></a-text>
+              );
+            }
+          }
+
+          if (item) {
+            return <>
+              <a-gltf-model
+                key={"model-" + item.id}
+                animation="property: rotation; to: 0 360 0; dur: 20000; easing: linear; loop: true"
+                src={`#${item.id}`}
                 rotation="0 0 0"
                 position={computePosition(position)}
+              ></a-gltf-model>
+              <a-text
+                key={"item-" + item.id}
+                value={item.name}
+                color="#000"
+                width="2"
+                rotation="0 -180 0"
+                position={computePosition({ ...position, level: position.level + 1 })}
               ></a-text>
-            );
+              <a-text
+                key={"item-" + item.id}
+                value={item.name}
+                color="#000"
+                width="2"
+                rotation="0 0 0"
+                position={computePosition({ ...position, level: position.level + 1 })}
+              ></a-text>
+            </>
           }
-        }
 
-        if (item) {
-          return <>
-            <a-gltf-model
-              key={"model-" + item.id}
-              animation="property: rotation; to: 0 360 0; dur: 20000; easing: linear; loop: true"
-              src={`#${item.id}`}
-              rotation="0 0 0"
-              position={computePosition(position)}
-            ></a-gltf-model>
-            <a-text
-              key={"item-" + item.id}
-              value={item.name}
-              color="#000"
-              width="2"
-              rotation="0 -180 0"
-              position={computePosition({ ...position, level: position.level + 1 })}
-            ></a-text>
-            <a-text
-              key={"item-" + item.id}
-              value={item.name}
-              color="#000"
-              width="2"
-              rotation="0 0 0"
-              position={computePosition({ ...position, level: position.level + 1 })}
-            ></a-text>
-          </>
-        }
-
-      })}
-       {/* <a-sky src="#sky"></a-sky> */}
-    </a-scene>
+        })}
+        {/* <a-sky src="#sky"></a-sky> */}
+      </a-scene>
+    </div>
   );
 };
