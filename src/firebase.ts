2db36937d81138351ae142b5e683aded5b5538d7
Max Bo <mjbo@canva.com>
2025-07-07 15:38:48 +1000

    Add Firebase

---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB58W8TQi0UnVCNvXcgCTc6P230DXzya44",
+  authDomain: "poppenhu-is.firebaseapp.com",
+  projectId: "poppenhu-is",
+  storageBucket: "poppenhu-is.firebasestorage.app",
+  messagingSenderId: "584295659368",
+  appId: "1:584295659368:web:297ae35ccd2653f68208dd",
+  measurementId: "G-450HM7SJ6H"
+};
+
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
