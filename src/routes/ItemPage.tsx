160a1a70c3af2439a3fe3352d757f39f294a6ed2
Max Bo <max@maxbo.me>
2025-11-28 13:52:41 -0500

    ref

--- src/routes/ItemPage.tsx
+++ src/routes/ItemPage.tsx
@@ -125,51 +125,51 @@ function DescriptionList(props: { item: Item; collection: Collection; user: User
       {customFields}
       {customFields && <hr className="break" />}
       {customFields && <hr className="break" />}
-      <dt><abbr title={ITEM_FIELD_SCHEMAS.formalName.description}>formal name</abbr></dt>
+      <dt><abbr title={ITEM_FIELD_SCHEMAS.formalName.description}>{ITEM_FIELD_SCHEMAS.formalName.label}</abbr></dt>
       <dd>{item.formalName}</dd>
-      {/* <dt><abbr title={ITEM_FIELD_SCHEMAS.alt.description}>alt</abbr></dt>
+      {/* <dt><abbr title={ITEM_FIELD_SCHEMAS.alt.description}>{ITEM_FIELD_SCHEMAS.alt.label}</abbr></dt>
       <dd>{item.alt}</dd> */}
-      <dt><abbr title={ITEM_FIELD_SCHEMAS.releaseDate.description}>release date</abbr></dt>
+      <dt><abbr title={ITEM_FIELD_SCHEMAS.releaseDate.description}>{ITEM_FIELD_SCHEMAS.releaseDate.label}</abbr></dt>
       <dd>{item.releaseDate}</dd>
       <hr className="break" />
       <hr className="break" />
-      <dt>manufacturer</dt>
+      <dt>{ITEM_FIELD_SCHEMAS.manufacturer.label}</dt>
       <dd>{item.manufacturer}</dd>
-      <dt>manufacture date</dt>
+      <dt>{ITEM_FIELD_SCHEMAS.manufactureDate.label}</dt>
       <dd>{item.manufactureDate}</dd>
-      <dt>manufacture location</dt>
+      <dt>{ITEM_FIELD_SCHEMAS.manufactureLocation.label}</dt>
       <dd>{item.manufactureLocation}</dd>
-      <dt>material</dt>
+      <dt>{ITEM_FIELD_SCHEMAS.material.label}</dt>
       <dd className='list'>{item.material?.join(", ")}</dd>
       <hr className="break" />
       <hr className="break" />
-      <dt>acquisition date</dt>
+      <dt>{ITEM_FIELD_SCHEMAS.acquisitionDate.label}</dt>
       <dd>{item.acquisitionDate}</dd>
-      <dt>acquisition location</dt>
+      <dt>{ITEM_FIELD_SCHEMAS.acquisitionLocation.label}</dt>
       <dd>{item.acquisitionLocation}</dd>
       <hr className="break" />
       <hr className="break" />
-      <dt>storage location</dt>
+      <dt>{ITEM_FIELD_SCHEMAS.storageLocation.label}</dt>
       <dd>{item.storageLocation}</dd>
       <hr className="break" />
       <hr className="break" />
-      <dt>capture date</dt>
+      <dt>{ITEM_FIELD_SCHEMAS.captureDate.label}</dt>
       <dd>{item.captureDate}</dd>
-      <dt>capture location</dt>
+      <dt>{ITEM_FIELD_SCHEMAS.captureLocation.label}</dt>
       <dd>{location}</dd>
-      <dt>capture device</dt>
+      <dt>{ITEM_FIELD_SCHEMAS.captureDevice.label}</dt>
       <dd>{item.captureDevice}</dd>
-      <dt>capture app</dt>
+      <dt>{ITEM_FIELD_SCHEMAS.captureApp.label}</dt>
       <dd>{item.captureApp}</dd>
-      <dt>capture method</dt>
+      <dt>{ITEM_FIELD_SCHEMAS.captureMethod.label}</dt>
       <dd>{item.captureMethod}</dd>
       <hr className="break" />
       <hr className="break" />
-      <dt><abbr title={ITEM_FIELD_SCHEMAS.model.description}>glTF model</abbr></dt>
+      <dt><abbr title={ITEM_FIELD_SCHEMAS.model.description}>{ITEM_FIELD_SCHEMAS.model.label}</abbr></dt>
       <dd className='ellipsis'><a href={item.model}>{item.model}</a></dd>
-      <dt><abbr title={ITEM_FIELD_SCHEMAS.usdzModel.description}>USDZ model</abbr></dt>
+      <dt><abbr title={ITEM_FIELD_SCHEMAS.usdzModel.description}>{ITEM_FIELD_SCHEMAS.usdzModel.label}</abbr></dt>
       <dd className='ellipsis'>{item.usdzModel && <a href={item.usdzModel}>{item.usdzModel}</a>}</dd>
-      <dt><abbr title={ITEM_FIELD_SCHEMAS.og.description}>Open Graph image</abbr></dt>
+      <dt><abbr title={ITEM_FIELD_SCHEMAS.og.description}>{ITEM_FIELD_SCHEMAS.og.label}</abbr></dt>
       <dd className='ellipsis'>{item.og && <a href={item.og}>{item.og}</a>}</dd>
     </dl>
   );
