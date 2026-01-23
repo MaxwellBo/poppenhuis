9be7da0531f395b981005ffe30fa67825aed0b88
Max Bo <max@maxbo.me>
2026-01-22 20:56:43 -0500

    NBSP

--- src/components/Size.tsx
+++ src/components/Size.tsx
@@ -2,5 +2,5 @@ export function Size(props: { ts: unknown[]; t: string; }) {
   const { ts, t } = props;
   const plural = t && ts && (ts.length > 1 || ts.length === 0) ? "s" : "";
 
-  return <span className='size'>({ts.length} {t}{plural})</span>;
+  return <span className='size'>({ts.length}&nbsp;{t}{plural})</span>;
 }
